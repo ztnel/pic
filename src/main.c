56428d1f494880af078a0a2bd0ff10d8e39f2b3a
ztnel <christian911@sympatico.ca>
2023-05-20 19:03:47 -0400

    demo main

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 
+#include <stdint.h>
 #include <xc.h>
 
+uint8_t add(uint8_t a, uint8_t b) {
+  return a + b;
+}
 int main(void) {
-  while (1) {}
+  uint8_t x, y, z;
+  x = 2;
+  y = 5;
+  z = add(x, y);
+  return 0;
 }
