a7be93cc9eae52881a890f41276e1109935d5c19
ztnel <christian911@sympatico.ca>
2023-05-22 00:18:02 -0400

    wait

--- src/main.c
+++ src/main.c
@@ -5,13 +5,7 @@
 #include "config.h"
 #include "configuration.h"
 
-uint8_t add(uint8_t a, uint8_t b) {
-  return a + b;
-}
 int main(void) {
-  uint8_t x, y, z;
-  x = 2;
-  y = 5;
-  z = add(x, y);
-  return 0;
+  while (1) {
+  }
 }
