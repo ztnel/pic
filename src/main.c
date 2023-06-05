5862e206244b02e89cad475474a23d0c59d0f005
ztnel <christian@leapsystems.online>
2023-06-05 13:00:11 -0400

    blink

--- src/main.c
+++ src/main.c
@@ -6,6 +6,22 @@
 #include "configuration.h"
 
 int main(void) {
+  TRISB = 0x00;
+  PORTB = 0x00;
+
   while (1) {
+    // Turn on RB0 to RB4 pins
+    PORTB = 0x01;
+
+    for (int i=0;i < 1000; i++){
+        
+    }
+
+    // Turn off RB0 to RB4 pins
+    PORTB = 0x00;
+
+    for (int i=0;i < 1000; i++){
+        
+    }
   }
 }
